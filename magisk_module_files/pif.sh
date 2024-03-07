680abbb21418667a318853c7f3feb5ea4b4263d8
Tesla <43712252+0x11DFE@users.noreply.github.com>
2024-03-07 16:26:00 +0100

    Attempt at fixing workflow duplicate check.
    This is an attempt at fixing the duplicate releases `check_build_id` by implementing a two side operand which checks for the module basename and build_id. Those are also specified on the module file name.
    
    Also added a way for the PIF to loop true known directory's in an attempt to remove issues such as having to copy the pif.json from adb into the modpath of `playintegrityfix`.

--- magisk_module_files/pif.sh
+++ magisk_module_files/pif.sh
@@ -2,7 +2,7 @@
 
 ui_print ""
 ui_print "- Building configuration file for PlayIntegrityFix"
-PIF_DIR="/data/adb/pif.json"
+PIF_DIRS="/data/adb/pif.json /data/adb/modules/playintegrityfix/pif.json"
 PIF_BUILD_ID=$(grep_prop "ro.product.build.id" "$MODPATH_SYSTEM_PROP")
 PIF_PRODUCT=$(grep_prop "ro.product.name" "$MODPATH_SYSTEM_PROP")
 PIF_DEVICE=$(grep_prop "ro.product.device" "$MODPATH_SYSTEM_PROP")
@@ -23,11 +23,13 @@ PIF_SDK=$(grep_prop "ro.build.version.sdk" "$MODPATH_SYSTEM_PROP")
 # Set ENABLE_PIF_SPOOF to false by default
 ENABLE_PIF_SPOOF=false
 
-# Check if the device name is beta or if ENABLE_PIF_SPOOF is set to true
-if [[ "$PIF_PRODUCT" == *_beta ]] || [ "$ENABLE_PIF_SPOOF" = "true" ]; then
+# Loop true each PIF dirs
+for PIF_DIR in $PIF_DIRS; do
+  # Check if the device name is beta or if ENABLE_PIF_SPOOF is set to true
+  if [[ "$PIF_PRODUCT" == *_beta ]] || [ "$ENABLE_PIF_SPOOF" = "true" ]; then
 
-  NEW_PIF=$(
-    cat <<EOF
+    NEW_PIF=$(
+      cat <<EOF
 {
   "BUILD_ID": "$PIF_BUILD_ID",
   "PRODUCT": "$PIF_PRODUCT",
@@ -41,41 +43,42 @@ if [[ "$PIF_PRODUCT" == *_beta ]] || [ "$ENABLE_PIF_SPOOF" = "true" ]; then
   "_comment": "https://t.me/PixelProps",
 }
 EOF
-  )
+    )
 
-  # Compare new PIF with existing PIF file
-  if echo "$NEW_PIF" | cmp - "$PIF_DIR" >/dev/null; then
-    ui_print " - No changes detected in PlayIntegrityFix file."
+    # Compare new PIF with existing PIF file
+    if echo "$NEW_PIF" | cmp - "$PIF_DIR" >/dev/null; then
+      ui_print " - No changes detected in PlayIntegrityFix file."
+    else
+      mv "$PIF_DIR" "${PIF_DIR}.old"
+      echo "$NEW_PIF" >"$PIF_DIR"
+      ui_print " -+ PlayIntegrityFix file has been updated and saved to $PIF_DIR"
+
+      # Kill and clear data from few Google apps
+      for google_app in $GOOGLE_APPS; do
+        am force-stop "$google_app"
+        # su -c pm clear "$google_app" # Before clearing the data we need TODO: Automate sign-out from Device Activity
+        # ui_print " ? Cleanned $google_app"
+      done
+
+      # Instructions
+      ui_print "  ? Please disconnect your device from your Google account: https://myaccount.google.com/device-activity"
+      ui_print "  ? Clean the data from Google system apps such as GMS, GSF, and Google apps"
+      ui_print "  ? Then restart and make sure to reconnect to your device, Make sure if your device is logged as \"$PIF_MODEL\"."
+      ui_print "  ? Remember to also disable any ROM internal PlayIntegrityFix spoofing e.g goolag.pif and use default module."
+
+      # Open the guide for fixing new generation assistant and possibly more...
+      nohup am start -a android.intent.action.VIEW -d https://t.me/PixelProps/157 >/dev/null 2>&1 &
+    fi
   else
-    mv "$PIF_DIR" "${PIF_DIR}.bak"
-    echo "$NEW_PIF" >"$PIF_DIR"
-    ui_print " -+ PlayIntegrityFix file has been updated and saved to $PIF_DIR"
+    ui_print " - PlayIntegritySpoof does not met device or is disabled."
 
-    # Kill and clear data from few Google apps
-    for google_app in $GOOGLE_APPS; do
-      am force-stop "$google_app"
-      # su -c pm clear "$google_app" # Before clearing the data we need TODO: Automate sign-out from Device Activity
-      # ui_print " ? Cleanned $google_app"
-    done
+    # If has backup restore it
+    [ -f "${PIF_DIR}.old" ] && mv "${PIF_DIR}.old" "$PIF_DIR"
 
-    # Instructions
-    ui_print "  ? Please disconnect your device from your Google account: https://myaccount.google.com/device-activity"
-    ui_print "  ? Clean the data from Google system apps such as GMS, GSF, and Google apps"
-    ui_print "  ? Then restart and make sure to reconnect to your device, Make sure if your device is logged as \"$PIF_MODEL\"."
-    ui_print "  ? Remember to also disable any ROM internal PlayIntegrityFix spoofing e.g goolag.pif and use default module."
-
-    # Open the guide for fixing new generation assistant and possibly more...
-    nohup am start -a android.intent.action.VIEW -d https://t.me/PixelProps/157 >/dev/null 2>&1 &
+    # If the pif.json is missing then we create one from maintained project
+    if [ ! -f "$PIF_DIR" ]; then
+      ui_print " -+ Missing $PIF_DIR, Downloading stable one for you."
+      wget -O -q --show-progress "$PIF_DIR" "https://raw.githubusercontent.com/x1337cn/AutoPIF-Next/main/pif.json"
+    fi
   fi
-else
-  ui_print " - PlayIntegritySpoof does not met device or is disabled."
-
-  # If has backup restore it
-  [ -f "${PIF_DIR}.bak" ] && mv "${PIF_DIR}.bak" "$PIF_DIR"
-
-  # If the pif.json is missing then we create one from maintained project
-  if [ ! -f "$PIF_DIR" ]; then
-    ui_print " -+ Missing $PIF_DIR, Downloading stable one for you."
-    wget -O -q --show-progress "$PIF_DIR" "https://raw.githubusercontent.com/x1337cn/AutoPIF-Next/main/pif.json"
-  fi
-fi
+done
